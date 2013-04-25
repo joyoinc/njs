6ca7dbbf62713e1d6db7c9fb162079c3bec4c40a
xxd <xxd@joyoedu.net>
2013-04-25 18:30:24 -0400

    use mongo

--- app-simple.js
+++ app-simple.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+//var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-var articleProvider= new ArticleProvider();
+var articleProvider= new ArticleProvider('localhost',27017);
+//var articleProvider= new ArticleProvider();
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
@@ -31,6 +33,40 @@ app.get('/', function(req, res){
             articles:docs
         });
     });
+});
+
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', {
+        title: 'New Post'
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade', {
+            title: article.title,
+            article:article
+        });
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
 });
 
 app.listen(3000);
